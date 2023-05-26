d606c4cd1dce2adbeb0b413848aa09f74d09f975
lengyue <lengyue@lengyue.me>
2023-05-26 01:37:20 -0400

    init ui

---
+++ rtvc/gui.py
@@ -0,0 +1,16 @@
+import sys
+
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+        button = QPushButton("Press Me!")
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+
